63972513a3366d250a4462585b1d5f480dff59e6
patildheeraj <dheerajpatil015@gmail.com>
2022-01-24 12:36:29 +0530

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,40 @@
+const { createStore } = require('vuex');
+
+import cartModule from './modules/cart';
+import productsModule from './modules/products';
+
+const store = createStore({
+  modules: {
+    prods: productsModule,
+    cart: cartModule,
+  },
+
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+  actions: {
+    login(context) {
+      context.commit('login');
+    },
+    logout(context) {
+      context.commit('logout');
+    },
+  },
+  getters: {
+    isAuthenticated(state) {
+      return state.isLoggedIn;
+    },
+  },
+});
+
+export default store;
